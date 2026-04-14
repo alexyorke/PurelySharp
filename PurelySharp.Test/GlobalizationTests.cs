b2e83620008abfafb355f5780372b510e68e728f
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 15:10:18 -0700

    Treat DateTime.ToShortDateString as impure

--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -1215,6 +1215,26 @@ public class TestClass
         }
 
         [Test]
+        public async Task DateTimeToShortDateString_CurrentCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public string {|PS0002:TestMethod|}(DateTime value)
+    {
+        return value.ToShortDateString();
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task DateOnlyParse_CurrentCulture_Diagnostic()
         {
             var test = @"
