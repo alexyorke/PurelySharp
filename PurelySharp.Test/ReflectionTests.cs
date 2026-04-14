15ce7aabe8fee6f11cbd137ac8ef80d6613be109
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 06:19:15 -0700

    Treat Type.IsNestedPublic as impure

--- PurelySharp.Test/ReflectionTests.cs
+++ PurelySharp.Test/ReflectionTests.cs
@@ -816,6 +816,24 @@ public class TestClass
         }
 
         [Test]
+        public async Task TypeIsNestedPublic_Diagnostic()
+        {
+            var test = @"
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}(System.Type type)
+    {
+        return type.IsNestedPublic;
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task TypeInfoGetFields_Diagnostic()
         {
             var test = @"
