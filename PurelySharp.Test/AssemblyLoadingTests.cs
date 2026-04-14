5502a80724aaf8df4c52a5b82a34a7eacf1da272
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 04:01:48 -0700

    Add an assembly manifest-resources smoke test

--- PurelySharp.Test/AssemblyLoadingTests.cs
+++ PurelySharp.Test/AssemblyLoadingTests.cs
@@ -156,6 +156,28 @@ namespace TestNamespace
         }
 
         [Test]
+        public async Task Assembly_GetManifestResourceNames_Diagnostic()
+        {
+            var test = @"
+using System.Reflection;
+using PurelySharp.Attributes;
+
+namespace TestNamespace
+{
+    public class TestClass
+    {
+        [EnforcePure]
+        public string[] {|PS0002:TestMethod|}(Assembly assembly)
+        {
+            return assembly.GetManifestResourceNames();
+        }
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task Assembly_LoadFile_NoDiagnostic()
         {
             var test = @"
