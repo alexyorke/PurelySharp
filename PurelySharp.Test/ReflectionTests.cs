5ecb19deeb23ed5c62a7ca85222b47902a589ccc
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 05:55:57 -0700

    Treat Type.IsClass as impure

--- PurelySharp.Test/ReflectionTests.cs
+++ PurelySharp.Test/ReflectionTests.cs
@@ -510,6 +510,24 @@ public class TestClass
         }
 
         [Test]
+        public async Task TypeIsClass_Diagnostic()
+        {
+            var test = @"
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}(System.Type type)
+    {
+        return type.IsClass;
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task TypeInfoGetFields_Diagnostic()
         {
             var test = @"
