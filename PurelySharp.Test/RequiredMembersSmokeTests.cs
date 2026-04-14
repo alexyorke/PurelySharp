e505bd9c6c31023aa6a7c9e5f9a39b6aa3def404
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-13 23:02:29 -0700

    Enable a required-members smoke test

---
+++ PurelySharp.Test/RequiredMembersSmokeTests.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PurelySharp.Analyzer;
+using VerifyCS = PurelySharp.Test.CSharpAnalyzerVerifier<
+    PurelySharp.Analyzer.PurelySharpAnalyzer>;
+
+namespace PurelySharp.Test
+{
+    [TestFixture]
+    public class RequiredMembersSmokeTests
+    {
+        [Test]
+        public async Task RequiredInitOnlyProperties_ReportPureGetterSuggestions()
+        {
+            var test = @"
+#nullable enable
+using PurelySharp.Attributes;
+
+namespace TestNamespace;
+
+public class Person
+{
+    public required string {|PS0004:FirstName|} { get; init; }
+    public required string {|PS0004:LastName|} { get; init; }
+
+    [EnforcePure]
+    public string GetFullName()
+    {
+        return $""{FirstName} {LastName}"";
+    }
+}
+";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+    }
+}
