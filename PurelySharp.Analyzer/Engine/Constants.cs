b9c71fae7b3e846216d26fe0922bab68c0819a8a
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 16:29:02 -0700

    Treat span DateTimeOffset.TryParse as impure

--- PurelySharp.Analyzer/Engine/Constants.cs
+++ PurelySharp.Analyzer/Engine/Constants.cs
@@ -400,6 +400,7 @@ public static class Constants
         "System.DateTime.ToString()",
         "System.DateTimeOffset.Parse(string)",
         "System.DateTimeOffset.TryParse(string, out System.DateTimeOffset)",
+        "System.DateTimeOffset.TryParse(System.ReadOnlySpan<char>, out System.DateTimeOffset)",
         "System.DateTimeOffset.ToString(string?)",
         "System.DateTimeOffset.ToString()",
         "System.Exception.ToString()",
--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -2059,6 +2059,27 @@ public class TestClass
         }
 
         [Test]
+        public async Task DateTimeOffsetTryParse_Span_CurrentCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}(string dateStr)
+    {
+        ReadOnlySpan<char> span = dateStr.AsSpan();
+        return DateTimeOffset.TryParse(span, out _);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task DateTimeOffsetToString_FormatString_CurrentCulture_Diagnostic()
         {
             var test = @"
