86d959eb454db385910f3a86189208f55cdc631b
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 10:20:30 -0700

    Treat Queue.ToArray as impure

--- PurelySharp.Test/CollectionArrayTests.cs
+++ PurelySharp.Test/CollectionArrayTests.cs
@@ -27,5 +27,24 @@ public class TestClass
 
             await VerifyCS.VerifyAnalyzerAsync(test);
         }
+
+        [Test]
+        public async Task QueueToArray_Diagnostic()
+        {
+            var test = @"
+using System.Collections.Generic;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public int[] {|PS0002:TestMethod|}(Queue<int> values)
+    {
+        return values.ToArray();
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
     }
 }
