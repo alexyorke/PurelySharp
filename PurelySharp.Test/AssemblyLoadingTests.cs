8ab5fafcdccb849827d3738fdcb41daa5ca97010
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 05:09:09 -0700

    Add a module resolve-signature smoke test

--- PurelySharp.Test/AssemblyLoadingTests.cs
+++ PurelySharp.Test/AssemblyLoadingTests.cs
@@ -995,6 +995,28 @@ namespace TestNamespace
         }
 
         [Test]
+        public async Task Module_ResolveSignature_Diagnostic()
+        {
+            var test = @"
+using System.Reflection;
+using PurelySharp.Attributes;
+
+namespace TestNamespace
+{
+    public class TestClass
+    {
+        [EnforcePure]
+        public byte[] {|PS0002:TestMethod|}(Module module)
+        {
+            return module.ResolveSignature(0);
+        }
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task Assembly_LoadFile_NoDiagnostic()
         {
             var test = @"
