5a895d4853a517d7fb3690015311160bf5e521bb
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 00:01:23 -0700

    Treat Environment.Is64BitOperatingSystem as impure

--- PurelySharp.Test/EnvironmentTests.cs
+++ PurelySharp.Test/EnvironmentTests.cs
@@ -96,5 +96,24 @@ public class TestClass
 
             await VerifyCS.VerifyAnalyzerAsync(test);
         }
+
+        [Test]
+        public async Task Environment_Is64BitOperatingSystem_Diagnostic()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}()
+    {
+        return Environment.Is64BitOperatingSystem;
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
     }
 }
