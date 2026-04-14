504c54187108403c68049b3556bac6eb122c3c6b
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-13 23:31:06 -0700

    Cover Random.Shared.NextInt64

---
+++ PurelySharp.Test/RandomOverloadTests.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PurelySharp.Analyzer;
+using VerifyCS = PurelySharp.Test.CSharpAnalyzerVerifier<
+    PurelySharp.Analyzer.PurelySharpAnalyzer>;
+
+namespace PurelySharp.Test
+{
+    [TestFixture]
+    public class RandomOverloadTests
+    {
+        [Test]
+        public async Task RandomSharedNextInt64_Diagnostic()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public long {|PS0002:TestMethod|}()
+    {
+        return Random.Shared.NextInt64();
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+    }
+}
