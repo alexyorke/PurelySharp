16a5c21181f01752cc9e4c6979e6ec9680920334
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 13:50:33 -0700

    Rename null check impurity regression

--- PurelySharp.Test/NullCheckTests.cs
+++ PurelySharp.Test/NullCheckTests.cs
@@ -66,7 +66,7 @@ public class TestClass
         }
 
         [Test]
-        public async Task PureMethodWithNullCheckAndImpureOperation_Diagnostic()
+        public async Task MethodWithNullCheckAndImpureOperation_Diagnostic()
         {
             var test = @"
 using System;
