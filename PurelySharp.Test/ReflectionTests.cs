f8ed52a20ac04ad539121cd93ba68f1e2fc3e18d
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 01:54:15 -0700

    Add a PropertyInfo.GetValue smoke test

--- PurelySharp.Test/ReflectionTests.cs
+++ PurelySharp.Test/ReflectionTests.cs
@@ -33,5 +33,37 @@ public class TestClass
 
             await VerifyCS.VerifyAnalyzerAsync(test);
         }
+
+        [Test]
+        public async Task PropertyInfoGetValue_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System.Reflection;
+using PurelySharp.Attributes;
+
+public class Data
+{
+    public int Value { get; set; }
+}
+
+public class TestClass
+{
+    [EnforcePure]
+    public object? TestMethod(PropertyInfo property, Data data)
+    {
+        return property.GetValue(data);
+    }
+}";
+
+            var expectedGetter = VerifyCS.Diagnostic(PurelySharpDiagnostics.MissingEnforcePureAttributeId)
+                .WithSpan(8, 16, 8, 21)
+                .WithArguments("get_Value");
+            var expectedMethod = VerifyCS.Diagnostic(PurelySharpDiagnostics.PurityNotVerifiedId)
+                .WithSpan(14, 20, 14, 30)
+                .WithArguments("TestMethod");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expectedGetter, expectedMethod);
+        }
     }
 }
