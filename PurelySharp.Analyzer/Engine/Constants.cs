32472709981407b04cfe22f62d90cfdecdc337f4
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 18:08:26 -0700

    Treat span DateTimeOffset.ParseExact as impure

--- PurelySharp.Analyzer/Engine/Constants.cs
+++ PurelySharp.Analyzer/Engine/Constants.cs
@@ -411,6 +411,7 @@ public static class Constants
         "System.DateTimeOffset.ParseExact(string, string, System.IFormatProvider?)",
         "System.DateTimeOffset.ParseExact(string, string, System.IFormatProvider?, System.Globalization.DateTimeStyles)",
         "System.DateTimeOffset.ParseExact(string, string[], System.IFormatProvider?, System.Globalization.DateTimeStyles)",
+        "System.DateTimeOffset.ParseExact(System.ReadOnlySpan<char>, System.ReadOnlySpan<char>, System.IFormatProvider?, System.Globalization.DateTimeStyles)",
         "System.DateTimeOffset.TryParse(string, out System.DateTimeOffset)",
         "System.DateTimeOffset.TryParseExact(string, string, System.IFormatProvider, System.Globalization.DateTimeStyles, out System.DateTimeOffset)",
         "System.DateTimeOffset.TryParseExact(string?, string?[]?, System.IFormatProvider?, System.Globalization.DateTimeStyles, out System.DateTimeOffset)",
--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -190,6 +190,28 @@ public class TestClass
         }
 
         [Test]
+        public async Task DateTimeOffsetParseExact_SpanSingleFormat_InvariantCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using System.Globalization;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public DateTimeOffset {|PS0002:TestMethod|}(string dateStr)
+    {
+        ReadOnlySpan<char> span = dateStr.AsSpan();
+        return DateTimeOffset.ParseExact(span, ""O"", CultureInfo.InvariantCulture, DateTimeStyles.None);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task TimeSpanParse_CurrentCulture_Diagnostic()
         {
             var test = @"
