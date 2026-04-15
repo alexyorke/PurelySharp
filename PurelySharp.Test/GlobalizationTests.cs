15da8fb89dd65346dcf0027f89687d40e5bf3e02
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 19:37:55 -0700

    Add span DateTime.ParseExact regression

--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -2503,6 +2503,28 @@ public class TestClass
         }
 
         [Test]
+        public async Task DateTimeParseExact_SpanSingleFormat_InvariantCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using System.Globalization;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public DateTime {|PS0002:TestMethod|}(string dateStr)
+    {
+        ReadOnlySpan<char> span = dateStr.AsSpan();
+        return DateTime.ParseExact(span, ""O"", CultureInfo.InvariantCulture, DateTimeStyles.None);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task DateTimeTryParse_CurrentCulture_Diagnostic()
         {
             var test = @"
