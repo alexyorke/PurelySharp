e363504d121b47240df21a460e1123a9cf787c83
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 19:46:37 -0700

    Add span TimeSpan.TryParse provider regression

--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -3828,6 +3828,28 @@ public class TestClass
         }
 
         [Test]
+        public async Task TimeSpanTryParse_SpanInvariantCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using System.Globalization;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}(string value)
+    {
+        ReadOnlySpan<char> span = value.AsSpan();
+        return TimeSpan.TryParse(span, CultureInfo.InvariantCulture, out _);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task TimeOnlyParse_CurrentCulture_Diagnostic()
         {
             var test = @"
