d516c390e267af80b2c18ff6d574f41896cceeaf
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 18:59:22 -0700

    Add DateOnly.ParseExact provider regression

--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -2657,6 +2657,27 @@ public class TestClass
         }
 
         [Test]
+        public async Task DateOnlyParseExact_InvariantCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using System.Globalization;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public DateOnly {|PS0002:TestMethod|}(string dateStr)
+    {
+        return DateOnly.ParseExact(dateStr, ""d"", CultureInfo.InvariantCulture);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task DateOnlyParseExact_MultipleFormatsInvariantCultureWithStyles_Diagnostic()
         {
             var test = @"
