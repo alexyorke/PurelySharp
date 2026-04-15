266b79c93e55468540705edbe771da80638f9fae
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 19:10:42 -0700

    Add span TimeSpan.TryParseExact array provider regression

--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -658,6 +658,28 @@ public class TestClass
         }
 
         [Test]
+        public async Task TimeSpanTryParseExact_SpanMultipleFormatsInvariantCulture_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using System.Globalization;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}(string value)
+    {
+        ReadOnlySpan<char> span = value.AsSpan();
+        return TimeSpan.TryParseExact(span, new[] { ""c"", ""g"" }, CultureInfo.InvariantCulture, out _);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task DoubleParse_InvariantCulture_NoDiagnostic()
         {
             var test = @"
