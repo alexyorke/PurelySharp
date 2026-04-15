ec1cd1003bbd4b14a9bfb5d39fc3cd703dcb393a
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-14 18:41:31 -0700

    Add DateTime.TryParse provider styles regression

--- PurelySharp.Test/GlobalizationTests.cs
+++ PurelySharp.Test/GlobalizationTests.cs
@@ -2160,6 +2160,27 @@ public class TestClass
         }
 
         [Test]
+        public async Task DateTimeTryParse_InvariantCultureWithStyles_Diagnostic()
+        {
+            var test = @"
+#nullable enable
+using System;
+using System.Globalization;
+using PurelySharp.Attributes;
+
+public class TestClass
+{
+    [EnforcePure]
+    public bool {|PS0002:TestMethod|}(string dateStr)
+    {
+        return DateTime.TryParse(dateStr, CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task DateTimeTryParse_Span_CurrentCulture_Diagnostic()
         {
             var test = @"
