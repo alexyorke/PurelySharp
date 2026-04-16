8aa8f2760b8bfe3bdee33974115445da3f40475e
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-04-15 21:54:08 -0700

    Mark static Regex.Split materialization as impure

--- PurelySharp.Test/RegexTests.cs
+++ PurelySharp.Test/RegexTests.cs
@@ -111,5 +111,25 @@ public class TestClass
 ";
             await VerifyCS.VerifyAnalyzerAsync(test);
         }
+
+        [Test]
+        public async Task Regex_Split_Static_Diagnostic()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
+using System.Text.RegularExpressions;
+
+public class TestClass
+{
+    [EnforcePure]
+    public string[] {|PS0002:TestMethod|}(string input)
+    {
+        return Regex.Split(input, ""[ ,;]"");
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
     }
 }
