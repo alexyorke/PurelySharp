7496a1c49fed17f3b13fc3a73edd09ae2a5f76e8
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-05-05 02:03:51 -0700

    Add shared callee caching smoke coverage

--- PurelySharp.Test/CachingTests.cs
+++ PurelySharp.Test/CachingTests.cs
@@ -13,11 +13,38 @@ namespace PurelySharp.Test
     [TestFixture]
     public class CachingTests
     {
+        [Test]
+        public async Task SharedImpureCallee_ReusedAcrossManyCallers_ReportsAllCallers()
+        {
+            var test = @"
+using System;
+using PurelySharp.Attributes;
 
+public class TestClass
+{
+    [EnforcePure]
+    public void {|PS0002:ImpureLeaf|}()
+    {
+        Console.WriteLine(""side effect"");
+    }
 
+    [EnforcePure]
+    public void {|PS0002:Caller1|}() => ImpureLeaf();
 
+    [EnforcePure]
+    public void {|PS0002:Caller2|}() => ImpureLeaf();
 
+    [EnforcePure]
+    public void {|PS0002:Caller3|}() => ImpureLeaf();
 
+    [EnforcePure]
+    public void {|PS0002:Caller4|}() => ImpureLeaf();
 
+    [EnforcePure]
+    public void {|PS0002:Caller5|}() => ImpureLeaf();
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
     }
 }
