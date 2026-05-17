9d2e03830e4f2094c3476297a1d2b2e54f212c66
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-05-17 15:22:20 -0700

    Cover property receiver reassignment

--- PurelySharp.Test/PropertyGetterDispatchTests.cs
+++ PurelySharp.Test/PropertyGetterDispatchTests.cs
@@ -132,6 +132,50 @@ public class TestClass
         }
 
         [Test]
+        public async Task InterfacePropertyGetter_OnLocalReassignedFromUnknownImplementation_Diagnostic()
+        {
+            var test = @"
+using PurelySharp.Attributes;
+
+public interface IReassignedCounter
+{
+    int Count { get; }
+}
+
+public sealed class SealedReassignedCounter : IReassignedCounter
+{
+    public int Count => 1;
+}
+
+public sealed class ImpureReassignedCounter : IReassignedCounter
+{
+    private int _reads;
+
+    public int Count
+    {
+        get
+        {
+            _reads++;
+            return _reads;
+        }
+    }
+}
+
+public class TestClass
+{
+    [EnforcePure]
+    public int {|PS0002:Read|}(IReassignedCounter unknown)
+    {
+        IReassignedCounter counter = new SealedReassignedCounter();
+        counter = unknown;
+        return counter.Count;
+    }
+}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Test]
         public async Task VirtualPropertyGetter_WithImpureOverride_Diagnostic()
         {
             var test = @"
