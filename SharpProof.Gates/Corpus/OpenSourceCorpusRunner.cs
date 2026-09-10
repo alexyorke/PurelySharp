1494253dd099b1d0dbf285eae844f545f8c87535
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2026-09-09 20:22:49 -0700

    Trim unused corpus preamble using

--- SharpProof.Gates/Corpus/OpenSourceCorpusRunner.cs
+++ SharpProof.Gates/Corpus/OpenSourceCorpusRunner.cs
@@ -27,7 +27,6 @@ internal static class OpenSourceCorpusRunner
             global using System.Collections.Generic;
             global using System.IO;
             global using System.Linq;
-            global using System.Net.Http;
             global using System.Threading;
             global using System.Threading.Tasks;
             """,
